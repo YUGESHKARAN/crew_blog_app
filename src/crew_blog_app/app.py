df0de16038a47f85cfbf8974816adfcb067d597c
YUGESHKARAN <yugeshkaran01@gmail.com>
2025-09-10 11:10:40 +0530

    PORT modified

--- src/crew_blog_app/app.py
+++ src/crew_blog_app/app.py
@@ -225,5 +225,5 @@ def get_required_inputs():
         "inputs": list(Content.__fields__.keys())
     })
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Use PORT env var or default to 5000
+    port = int(os.environ.get("PORT", 6600))  # Use PORT env var or default to 5000
     app.run(host="0.0.0.0", port=port, debug=True)
